6caca27b24f81b4b4c7acd18ea46635136277c65
Joerg Roth <joerg.roth@geo.uzh.ch>
2022-06-29 16:31:40 +0200

    prettier

--- lib/Models/BaseMaps/defaultBaseMaps.ts
+++ lib/Models/BaseMaps/defaultBaseMaps.ts
@@ -4,7 +4,7 @@ import { BaseMapJson } from "./BaseMapsModel";
 export function defaultBaseMaps(terria: Terria): BaseMapJson[] {
   const baseMaps: BaseMapJson[] = [];
 
-/*
+  /*
   if (
     terria.configParameters.bingMapsKey &&
     !terria.configParameters.useCesiumIonBingImagery
