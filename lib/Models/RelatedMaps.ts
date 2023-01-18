e5f87db53a7707a39d07628064ebb189b157ce2e
Joerg Roth <joerg.roth@geo.uzh.ch>
2023-01-18 11:19:50 +0100

    update related maps

--- lib/Models/RelatedMaps.ts
+++ lib/Models/RelatedMaps.ts
@@ -14,4 +14,12 @@ export const defaultRelatedMaps: RelatedMap[] = [
     description:
       "The top destination for Megaliths and Prehistory worldwide. World-wide Ancient Site Database, Photos and Prehistoric Archaeology News with geolocation."
   },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Antarctica_%28orthographic_projection%29.svg/512px-Antarctica_%28orthographic_projection%29.svg.png",
+    url: "https://antarctica.hidden-history.ch",
+    title: "Antarctica and beyond",
+    description:
+      "Map and timeline of Antarctica"
+  },
 ];
